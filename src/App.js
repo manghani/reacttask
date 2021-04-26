1ac53c8ab93b25d1cfbf6989b19891dea8c1ea62
manghani <manghani.sourav9@gmail.com>
2021-04-26 17:57:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Textbox from './text'
+
+
+function App() {
+  return (
+    <div className="App">
+       <Textbox/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/text.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+
+
+const Textbox = () => {
+
+    // var x = 'red'
+    // var y = 50
+    let [colour, setcolour] = useState('red');
+    let [font, setfont] = useState(50);
+
+    const handleclick = (event) => {
+            setcolour(colour.target.value);
+            setfont(50)
+    }
+    function getcolour(colour){
+        setcolour(colour.target.value)
+    
+    }
+    function getfont(font){
+        setfont(font.target.value)
+    }
+    return ( 
+        <nav className="textbox">
+            <h1>Home</h1>
+            <div className="links">
+                
+                    <label>Colour
+                        <input type="text" onChange={getcolour} />
+                    </label>
+                    <label>Font
+                        <input type="text" onChange={getfont} />
+                    </label>
+                    <button onClick={handleclick}>Submit</button>
+                
+                <p style={{color: colour, fontSize: font}}>Hello this is a paragraph</p>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Textbox;
